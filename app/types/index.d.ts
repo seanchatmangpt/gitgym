962f96afaf604d81f9364eb149c1e5a2b7e16548
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2024-12-18 12:20:15 -0800

    Initial commit

---
+++ app/types/index.d.ts
@@ -0,0 +1,57 @@
+import type { ParsedContent } from '@nuxt/content'
+import type { Avatar, Badge, Link } from '#ui/types'
+
+export interface BlogPost extends ParsedContent {
+  title: string
+  description: string
+  date: string
+  image?: HTMLImageElement
+  badge?: Badge
+  authors?: ({
+    name: string
+    description?: string
+    avatar: Avatar
+  } & Link)[]
+}
+
+export type UserStatus = 'subscribed' | 'unsubscribed' | 'bounced'
+
+export interface User {
+  id: number
+  name: string
+  email: string
+  avatar?: Avatar
+  status: UserStatus
+  location: string
+}
+
+export interface Mail {
+  id: number
+  unread?: boolean
+  from: User
+  subject: string
+  body: string
+  date: string
+}
+
+export interface Member {
+  name: string
+  username: string
+  role: 'member' | 'owner'
+  avatar: Avatar
+}
+
+export interface Notification {
+  id: number
+  unread?: boolean
+  sender: User
+  body: string
+  date: string
+}
+
+export type Period = 'daily' | 'weekly' | 'monthly'
+
+export interface Range {
+  start: Date
+  end: Date
+}
